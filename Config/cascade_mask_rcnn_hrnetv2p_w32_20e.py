95a2e135d78e048e40ba661b1396b1d04989b47d
marcelodiaz558 <marcelodiaz558@hotmail.com>
2020-12-23 09:59:05 -0400

    fixed syntax error v1 config file

--- Config/cascade_mask_rcnn_hrnetv2p_w32_20e.py
+++ Config/cascade_mask_rcnn_hrnetv2p_w32_20e.py
@@ -228,17 +228,17 @@ data = dict(
         type=dataset_type,
         ann_file='./files/cell_annotations.json',
         img_prefix='./data/tabmod/',
-        pipeline=train_pipeline)
-    """val=dict(
-        type=dataset_type,
-        ann_file=data_root + 'VOC2007/test.json',
-        img_prefix=data_root + 'VOC2007/Test/',
-        pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'VOC2007/test.json',
-        img_prefix=data_root + 'VOC2007/Test/',
-        pipeline=test_pipeline))"""
+        pipeline=train_pipeline))
+"""val=dict(
+    type=dataset_type,
+    ann_file=data_root + 'VOC2007/test.json',
+    img_prefix=data_root + 'VOC2007/Test/',
+    pipeline=test_pipeline),
+test=dict(
+    type=dataset_type,
+    ann_file=data_root + 'VOC2007/test.json',
+    img_prefix=data_root + 'VOC2007/Test/',
+    pipeline=test_pipeline))"""
 # evaluation = dict(interval=1, metric=['bbox'])
 # optimizer
 optimizer = dict(type='SGD', lr=0.0012, momentum=0.9, weight_decay=0.0001)
